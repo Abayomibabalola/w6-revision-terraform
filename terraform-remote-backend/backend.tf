6874e004f8e6708e513ce2a1aa5ed5f8c8d21dd8
abayomibabalola <abayomibabalola001@gmail.com>
2025-02-07 13:03:33 -0600

    change

--- terraform-remote-backend/backend.tf
+++ terraform-remote-backend/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket = "week6-ab-bucket-terraform"
+    bucket = ""
     key    = "dev/terrafform.tfstate"
     region = "us-east-1"
-    dynamodb_table = "terraform-lock"
+    dynamodb_table = ""
     encrypt = true
   }
 }
